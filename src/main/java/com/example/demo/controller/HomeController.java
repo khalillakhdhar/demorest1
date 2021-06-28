bb3a6bd1d0ae87653b8038a8cfc478a198335786
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-06-28 10:12:25 +0100

    Update HomeController.java

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -2,13 +2,20 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class HomeController {
 	@GetMapping("")
-	String types()
+	public String types()
 	{
 		return "les types des  <a href='controles'>controllers</a>";
+	}
+	@GetMapping("controles")
+	public ModelAndView controles()
+	{
+		
+		return null;
 	}
 
 }
