3cd7c553079fb318fd9b308130d881a5be1bbc54
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-06-28 10:10:59 +0100

    Update HomeController.java

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,8 +1,14 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+	@GetMapping("")
+	String types()
+	{
+		return "les types des  <a href='controles'>controllers</a>";
+	}
 
 }
